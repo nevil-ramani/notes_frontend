6ecbe7da2294feafdd89f9264dbbb5a061311f64
nevil ramani <nevilramani20@gmail.com>
2023-03-16 01:33:09 +0530

    move app.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import axios from 'axios';
-import notesStore from './stores/notesStore';
+import notesStore from '../stores/notesStore';
 
 
 function App() {
